c9e670210d6898e59791eff0e9e087f9b95e086d
Saumya Verma <saumyaverma@kpmg.com>
2022-04-19 12:32:11 +0530

    Used shopping points as a factor instead of choice_count in level 1

--- level1.py
+++ level1.py
@@ -12,7 +12,7 @@ def level1(name, xp, shopping_points):
 
 
     print("========== LEVEL 1 ==========")
-    print("Now, choose 3 items from the item list below that you will be taking with you on this challenge")
+    print("Now, You have 100 points, choose items from the item list below that you will be taking with you on this challenge using those points")
 
     item_bag = {
         "item1" : "Medical Kit",
@@ -31,8 +31,7 @@ def level1(name, xp, shopping_points):
     item_list = []
 
     
-    choice_count = 0
-    while choice_count < 3:
+    while shopping_points >= 15:
         
         print("Please choose an item you want: ")
         choice = input("A or B or C or D : ")
@@ -42,30 +41,37 @@ def level1(name, xp, shopping_points):
             shopping_points = shopping_points - 15
             print("You have " + str(shopping_points) + " points left ")
         elif choice == "B" or choice.lower().strip() == "b":
-            print("You chose " + item_bag["item2"])
-            item_list.append(item_bag["item2"])
-            shopping_points = shopping_points - 25
-            print("You have " + str(shopping_points) + " points left ")
+            if(shopping_points < 25):
+                print("You don't have enough points to buy this")
+            else:
+                print("You chose " + item_bag["item2"])
+                item_list.append(item_bag["item2"])
+                shopping_points = shopping_points - 25
+                print("You have " + str(shopping_points) + " points left ")
         elif choice == "C" or choice.lower().strip() == "c":
-            print("You chose " + item_bag["item3"])
-            item_list.append(item_bag["item3"])
-            shopping_points = shopping_points - 30
-            print("You have " + str(shopping_points) + " points left ")
+            if(shopping_points < 30):
+                print("You don't have enough points to buy this")
+            else:
+                print("You chose " + item_bag["item3"])
+                item_list.append(item_bag["item3"])
+                shopping_points = shopping_points - 30
+                print("You have " + str(shopping_points) + " points left ")
         elif choice == "D" or choice.lower().strip() == "d":
-            print("You chose " + item_bag["item4"])
-            item_list.append(item_bag["item4"])
-            shopping_points = shopping_points - 20
-            print("You have " + str(shopping_points) + " points left ")
+            if(shopping_points < 20):
+                print("You don't have enough points to buy this")
+            else:
+                print("You chose " + item_bag["item4"])
+                item_list.append(item_bag["item4"])
+                shopping_points = shopping_points - 20
+                print("You have " + str(shopping_points) + " points left ")
         else:
             print("Please enter a letter between A and D")
-            choice_count -= 1
 
         print("=================================")
-        choice_count += 1
 
     friendly_itemlist = ', '.join(item_list)
     print("Your bag has - " + friendly_itemlist) # user friendly format list
-    print("Points - " + str(shopping_points) + " points")
+    print("Points left - " + str(shopping_points) + " points")
     print("XP - " + str(xp) + " XP")
     print("=================================")
 
