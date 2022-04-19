1154b9e186008fde820d8a22da734373cf2d7671
Saumya Verma <saumyaverma@kpmg.com>
2022-04-19 12:38:37 +0530

    Added placeholders for the remaining levels

---
+++ level7.py
@@ -0,0 +1,7 @@
+import os
+from level8 import *
+
+def level7(name, xp, shopping_points):
+
+    print(name, xp, shopping_points)
+    level8(name, xp, shopping_points)
---
+++ level8.py
@@ -0,0 +1,6 @@
+import os
+
+def level8(name, xp, shopping_points):
+
+    print(name, xp, shopping_points)
+    print("END!")
